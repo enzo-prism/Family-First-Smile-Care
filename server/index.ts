a08329e4371de75bb47359f9560fda8e6509ed5b
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2025-11-06 10:17:09 -0800

    Guard HTTP reusePort flag

--- server/index.ts
+++ server/index.ts
@@ -159,12 +159,14 @@ Sitemap: https://famfirstsmile.com/sitemap.xml`);
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
+  const port = parseInt(process.env.PORT || "5000", 10);
+  const listenOptions = {
     port,
     host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+    ...(process.platform === "linux" ? { reusePort: true } : {}),
+  };
+
+  server.listen(listenOptions, () => {
     log(`serving on port ${port}`);
   });
 })();
